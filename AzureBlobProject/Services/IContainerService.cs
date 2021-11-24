bf528e9ffd3cee8c28aba9d71c50185a0ca10ae6
demir005 <demirmah93@gmail.com>
2021-11-24 09:01:16 +0100

    Add project files.

---
+++ AzureBlobProject/Services/IContainerService.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureBlobProject.Services
+{
+    public interface IContainerService
+    {
+        Task <List<string>> GetAllContainerAndBlob();
+        Task<List<string>> GetAllContainer();
+        Task CreateContainer(string containerName);
+        Task DeleteContainer(string containerName);
+    }
+}
